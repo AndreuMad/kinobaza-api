2836bfdb5b4d57d28248a67ab09828d405b09263
andriy.labyk <andriy.labyk@thinkmobiles.com>
2017-12-08 17:10:11 +0300

    image

--- app/routes/actors.js
+++ app/routes/actors.js
@@ -2,64 +2,64 @@ const postActorQuery = require('../queries/actors/postActor');
 const getActorsQuery = require('../queries/actors/getActors');
 const likeActorQuery = require('../queries/actors/likeActor');
 
-const postActor = function(req, res) {
+const postActor = function (req, res) {
 
-    const requestBody = req.body;
-    const query = {
-        name: requestBody.name,
-        type: 'actor',
-        image: requestBody.image,
-        zodiacSign: requestBody.zodiacSign,
-        birthLocation: requestBody.birthLocation
-    };
+  const requestBody = req.body;
+  const query = {
+    name: requestBody.name,
+    type: 'actor',
+    image: requestBody.image,
+    zodiacSign: requestBody.zodiacSign,
+    birthLocation: requestBody.birthLocation
+  };
 
-    postActorQuery(query)
-        .then(() => res.json({ message: 'Actor created' }))
-        .catch(error => res.send(error));
+  postActorQuery(query)
+    .then(() => res.json({ message: 'Actor created' }))
+    .catch(error => res.send(error));
 };
 
-const getActors = function(req, res) {
-    const requestQuery = req.query;
+const getActors = function (req, res) {
+  const requestQuery = req.query;
 
-    const userId = requestQuery.userId;
-    const name = requestQuery.name;
-    const limit = +requestQuery.limit || 3;
-    const skip = +requestQuery.skip || 0;
-    let query = {};
+  const userId = requestQuery.userId;
+  const name = requestQuery.name;
+  const limit = +requestQuery.limit || 3;
+  const skip = +requestQuery.skip || 0;
+  let query = {};
 
-    if(name) {
-        query = Object.assign(
-            {
-                $or: [
-                    { "name.en": { "$regex": name, "$options": "i" } },
-                    { "name.ukr": { "$regex": name, "$options": "i" } }
-                ]
-            },
-            query
-        )
-    }
+  if (name) {
+    query = Object.assign(
+      {
+        $or: [
+          { "name.en": { "$regex": name, "$options": "i" } },
+          { "name.ukr": { "$regex": name, "$options": "i" } }
+        ]
+      },
+      query
+    )
+  }
 
-    getActorsQuery({ userId, query, skip, limit })
-        .then(result => {
+  getActorsQuery({ userId, query, skip, limit })
+    .then(result => {
 
-            res.json({
-                total: result.length ? result[0].total : 0,
-                likes: result.length ? result[0].likes : [],
-                actors: result
-            });
-        })
-        .catch(error => res.send(error));
+      res.json({
+        total: result.length ? result[0].total : 0,
+        likes: result.length ? result[0].likes : [],
+        actors: result
+      });
+    })
+    .catch(error => res.send(error));
 };
 
-const postLike = function(req, res) {
-    const { userId, actorId } = req.body;
-    likeActorQuery({ userId, actorId })
-        .then(result => res.send(result))
-        .catch(error => res.send(error));
+const postLike = function (req, res) {
+  const { userId, actorId } = req.body;
+  likeActorQuery({ userId, actorId })
+    .then(result => res.send(result))
+    .catch(error => res.send(error));
 };
 
 module.exports = {
-    postActor: postActor,
-    getActors: getActors,
-    postLike: postLike
+  postActor: postActor,
+  getActors: getActors,
+  postLike: postLike
 };
--- app/routes/authentication.js
+++ app/routes/authentication.js
@@ -6,66 +6,68 @@ const config = require('../../config');
 const getUserQuery = require('../queries/user/getUserQuery');
 
 function tokenForUser(user) {
-    const timestamp = new Date().getTime();
-    return jwt.encode({ sub: user.id, iat: timestamp }, config.secret);
+  const timestamp = new Date().getTime();
+  return jwt.encode({ sub: user.id, iat: timestamp }, config.secret);
 }
 
-const signup = function(req, res, next) {
-    const email = req.body.email;
-    const name = req.body.name;
-    const password = req.body.password;
+const signup = function (req, res, next) {
+  const email = req.body.email;
+  const name = req.body.name;
+  const password = req.body.password;
 
-    if(!email || !name || !password) {
-        return res.status(422)
-            .send({ error: "Please, fill all fields" });
+  if (!email || !name || !password) {
+    return res.status(422)
+      .send({ error: "Please, fill all fields" });
+  }
+
+  User.findOne({
+    email: email
+  }, function (error, existingUer) {
+    if (error) {
+      return next(error);
     }
 
-    User.findOne({
-        email: email
-    }, function(error, existingUer) {
-        if(error) {
-            return next(error);
-        }
+    if (existingUer) {
+      return res.status(422).send({ error: 'Email is in use' });
+    }
 
-        if(existingUer) {
-            return res.status(422).send({ error: 'Email is in use' });
-        }
-
-        const user = new User({
-            email: email,
-            name: name,
-            password: password
-        });
-
-        user.save(function(error) {
-            if(error) {
-                return next(error);
-            }
-
-            res.json({
-                id: user._id,
-                name: user.name,
-                token: tokenForUser(user)
-            });
-        });
+    const user = new User({
+      email: email,
+      name: name,
+      password: password
     });
+
+    user.save(function (error) {
+      if (error) {
+        return next(error);
+      }
+
+      res.json({
+        user : {
+          id: user._id,
+          name: user.name
+        },
+        token: tokenForUser(user)
+      });
+    });
+  });
 };
 
-const signin = function(req, res, next) {
-    // User has already had their email and password auth'd
-    // We just need to give them a token
-    res.send({
-        user: req.user,
-        token: tokenForUser(req.user)
-    });
+const signin = function (req, res, next) {
+  // User has already had their email and password auth'd
+  // We just need to give them a token
+  res.send({
+    user: req.user,
+    token: tokenForUser(req.user)
+  });
 };
 
-const signToken = function(req, res, next) {
-    res.send(req.user);
+const signToken = function (req, res, next) {
+  res.send(req.user);
 };
 
 module.exports = {
-    signup: signup,
-    signin: signin,
-    signToken: signToken
+  signup: signup,
+  signin: signin,
+  signToken: signToken
 };
